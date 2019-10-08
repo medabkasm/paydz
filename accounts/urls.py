078877e295447136f691839a259af849491be997
mohamed <medabkasm@gmail.com>
2019-10-08 22:05:31 +0100

    solving problem confirmation link

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path , include , reverse_lazy
+from django.urls import path , include , reverse_lazy , re_path
 from .views import *
 from django.contrib.auth import views as auth_views
 
 
 app_name = 'accounts'
 urlpatterns = [
-    path('register/activate/<uidb64>/<token>/',user_activation_view , name = 'activate'),
+    #path('register/activate/<uidb64>/<token>/',user_activation_view , name = 'activate'),
+    re_path(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', user_activation_view , name = 'activate'),
     path('register/',RegisterView.as_view(),name = 'register'),
     path('login/',auth_views.LoginView.as_view(template_name = 'accounts/registration/login.html' , redirect_authenticated_user = True),name = 'login'),
     path('logout/',auth_views.LogoutView.as_view(template_name = 'accounts/registration/logged_out.html'),name = 'logout'),
