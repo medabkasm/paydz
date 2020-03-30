2e99973b48aee7580d45904a06397834f9ed5d0f
mohamed abkasm <51716754+medabkasm@users.noreply.github.com>
2020-03-30 12:16:46 +0100

    Update env_variables.js

--- paysera/env_variables.js
+++ paysera/env_variables.js
@@ -1,27 +1,27 @@
 [
     {
-        "SECRET_KEY":"=jlhx*_k57wx6g3l1av_efdvl#=+&9@bihsfdff018go8w6d@&",
-        "EMAIL_HOST_USER":"medabkasm@gmail.com",
-        "EMAIL_HOST_PASSWORD":"nvbq mthi oszi hesn",
+        "SECRET_KEY":"=#",
+        "EMAIL_HOST_USER":"#",
+        "EMAIL_HOST_PASSWORD":"#",
 
-        "CAPTCHA_SITE":"6Ld2HbwUAAAAAKPzva1ucFAxSVGNU3gWUy31s433",
-        "GOOGLE_RECAPTCHA_SECRET_KEY":"6Ld2HbwUAAAAANPHOsoG4Zn9OrFi__up_ZuEqksa",
+        "CAPTCHA_SITE":"#",
+        "GOOGLE_RECAPTCHA_SECRET_KEY":"#",
 
-        "SOCIAL_AUTH_GOOGLE_OAUTH2_KEY":"513168660813-f66rug1mfh8lm25rkjjmlnrqp3l3bobn.apps.googleusercontent.com",
-        "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET":"622NhzhKmSfHBpA63JMeBvMN",
+        "SOCIAL_AUTH_GOOGLE_OAUTH2_KEY":"#",
+        "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET":"#",
 
-        "SOCIAL_AUTH_FACEBOOK_KEY":"1046190595567209",
-        "SOCIAL_AUTH_FACEBOOK_SECRET":"893a185dc1fd62bf93d771becf1aea23",
+        "SOCIAL_AUTH_FACEBOOK_KEY":"#",
+        "SOCIAL_AUTH_FACEBOOK_SECRET":"#",
 
 
-        "CAPTCHA_SITE_local":"6LcVKbwUAAAAAHCOxauF-kp0I6DyYXchHaB-ws-H",
-        "GOOGLE_RECAPTCHA_SECRET_KEY_local":"6LcVKbwUAAAAABBGNohDdyorgm7zRLuxHSKfIME2",
+        "CAPTCHA_SITE_local":"#",
+        "GOOGLE_RECAPTCHA_SECRET_KEY_local":"#",
 
-        "SOCIAL_AUTH_GOOGLE_OAUTH2_KEY2":"513168660813-f66rug1mfh8lm25rkjjmlnrqp3l3bobn.apps.googleusercontent.com",
-        "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET2":"622NhzhKmSfHBpA63JMeBvMN",
+        "SOCIAL_AUTH_GOOGLE_OAUTH2_KEY2":"#",
+        "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET2":"#",
 
-        "SOCIAL_AUTH_FACEBOOK_KEY_local":"373983913273604",
-        "SOCIAL_AUTH_FACEBOOK_SECRET_local":"ea43f2d089e4a40244013074dd3d5f91"
+        "SOCIAL_AUTH_FACEBOOK_KEY_local":"#",
+        "SOCIAL_AUTH_FACEBOOK_SECRET_local":"#"
 
     }
 ]
